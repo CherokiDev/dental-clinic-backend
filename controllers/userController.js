82b959de8730b355865b99df10ab93258d43dfbf
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2020-10-15 17:23:05 +0200

    implemented use of bcrypt for user registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,38 @@
 const UserModel = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const UserController = {
     async singup(req, res) {
+        let regExEmail = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/;
+        let regExPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+        let hashPass = await bcrypt.hash(req.body.password, 10);
+
+        if (!regExEmail.test(req.body.email)) {
+            res.send({
+                message: "El email introducido no es válido"
+            });
+            return;
+        }
+
+        if (!regExPassword.test(req.body.password)) {
+            res.send({
+                message: "El password introducido no es válido"
+            });
+            return;
+        }
+
         try {
-            const user = await UserModel.create(req.body);
-            res.status(201).send(user);
+            const user = await new UserModel({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: hashPass
+            }).save();
+
+            res.status(201).send({
+                message: 'Account created successfully.',
+                user
+            });
         } catch (error) {
             console.error(error);
             res.status(500).send({
@@ -14,9 +42,8 @@ const UserController = {
         }
     },
     async getUsers(req, res) {
-        try{
-            const users = await UserModel.find({
-            });
+        try {
+            const users = await UserModel.find({});
             res.send(users);
         } catch (error) {
             console.error(error);
@@ -26,14 +53,14 @@ const UserController = {
             })
         }
     },
-   /* getUsers = (req, res) => {
-        UserModel.find({})
-            .then(users => {
-                res.send(users)
-            })
-            .catch(error => console.log(error))
+    /* getUsers = (req, res) => {
+         UserModel.find({})
+             .then(users => {
+                 res.send(users)
+             })
+             .catch(error => console.log(error))
 
-    }*/
+     }*/
 }
 
 module.exports = UserController;
